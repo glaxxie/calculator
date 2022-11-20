8f6ee961563c0cfa93ee7d7435af0cca92cad5ed
glaxxie <mrt3@rocketmail.com>
2022-11-20 04:15:42 -0600

    Calculation logic finished and working. Need to do some work to display float, and big number.

--- script.js
+++ script.js
@@ -33,56 +33,136 @@ const buttons = document.querySelectorAll('button');
 
 //in order to limit number to len of 9 digits, split number and sign to 2 different var
 let curValue = '';
-let sign = '';
-let operatorInStack = false;
-let firstNum = secondNum = 0;
+// let sign = '';
+let opStack = [];
+// let firstNum = secondNum = 0;
+// let currentOperator = '';
 
 //helper functions
-const displayFullValue = () => display_box.textContent = `${sign}${+curValue}`
-const checkLen = numString => numString.length < 9;
+const printDebug = () => {
+    console.log(`Current val: ${curValue}`)
+    console.log(`Current stack: ${opStack}`)
+}
+const refreshVars = (stack=false) => {
+    if (stack) opStack.length = 0
+    curValue = '';
+} 
+const displayFullValue = () => {
+    // console.log(`curVal : ${curValue} type: ${typeof(curValue)} with len ${curValue.length}`)
+    //TODO: need to fix display of float, +/- and choose a number len
+    curValue.length >= 10 ? display_box.textContent = `err:toolarge`:
+    display_box.textContent = `${+curValue}`;
+}
+const checkLen = numString => numString.length < 10;
 
-const pressNumber = button => {
-    if (checkLen(curValue)) {
-        curValue += button.textContent;
-        displayFullValue();
+
+//function for buttons
+//passed
+const calculation = (num1,op,num2) => {
+    switch (op) {
+        case "+":
+            return add(num1, num2)
+        case "-":
+            return subtract(num1, num2)
+        case "*":
+            return multiply(num1, num2)
+        case "/":
+            return divide(num1, num2)
+        default:
+            break;
     }
 }
 
+const pressNumber = button => {
+    if (opStack.length === 1) {
+        //clear method
+        opStack.length = 0,
+        curValue = '';
+    }
+    if (checkLen(curValue)) {
+        curValue+= +(button.textContent);
+    }
+    // printDebug();
+    displayFullValue();
+}
+//passed
 const pressDecimal = button => {
     if (checkLen(curValue)) {
         if (curValue.includes('.')) button.disabled;
         else curValue+= ".";
-        displayFullValue();
     }
-}
-
-const pressSign = () => {
-    sign ? sign = "" : sign = "-";
+    // printDebug();
     displayFullValue();
 }
+//passed
+const pressSign = () => {
+    curValue.includes("-") ? curValue = curValue.replace("-",'') :
+    curValue = `-${+curValue}`
+    // printDebug();
+    displayFullValue();
+}
+//passes
 //2 buttons will clear the screen, but only clear remove the stacks and all values so far
 const pressClear = button => {
-    if (button.id === "clear") firstNum = secondNum = operatorInStack = 0;
-    curValue = sign = operatorInStack = '';
+    if (button.id === "clear") refreshVars(true);
+    refreshVars();
     displayFullValue();
+    // printDebug();
 };
 
+
+// 
 const pressEval = button => {
-    //if no operator on the stack, firstNum is the current value. display current value
-
-    //if there is operator on the stack. and second value not input yet. firstNum and secondNum is current value. current value is now firstNum
-
-    //if there is operator on the stack. evaluate first and second with operate. current value is now firstNum
-
+    if (opStack.length === 0) opStack.push(+curValue)
+    else if (opStack.length === 1) { 
+        //pass
+    } else if (opStack.length === 2) {
+        let firstNum    , 
+            operator    ,
+            secondNum   ;
+        if (curValue === "") {
+            
+            [firstNum, operator, secondNum] = [+opStack[0],opStack[1],+opStack[0]];
+            console.log(firstNum, operator, secondNum)
+        } else {
+            opStack.push(+curValue);
+            [firstNum, operator, secondNum] = [...opStack];
+            console.log(firstNum, operator, secondNum)
+        }
+        curValue = calculation(firstNum, operator, secondNum).toString()
+        opStack.length = 0;
+        opStack.push(curValue);
+    }
+    displayFullValue();
+    // printDebug();
 }
 
-//
+
 const pressOperator = button => {
-
+    //if there is an operator on the stack. replace it
+    const operator = button.textContent
+    //when press add the current val to the stack
+    //if last value in the stack is not a number. pop and add the new one
+    if (opStack.length === 0) {
+        opStack.push(+curValue,operator) //passes
+    } else if (opStack.length === 1) {
+        opStack.push(operator)          //passes
+    } else if (opStack.length === 2) {
+        if (curValue === "") {
+            opStack[opStack.length - 1] = operator
+        } else {
+            pressEval();
+            opStack.push(operator)
+        }
+    }
+    curValue = "";
+    // sign = "";
+    // printDebug();
 }
+//
 
 
-
+//add event listner to buttons
 buttons.forEach( (button) => {
     button.addEventListener('click', () => {
         switch (button.className) {
